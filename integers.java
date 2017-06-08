52ea2acc8583711e9ab4ff6f82c628bddf6bc3a4
Kiruthika1996 <v.ammu79@gmail.com>
2017-06-08 17:44:31 +0530

    Create integers.java

---
+++ integers.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class integers
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+int n=scan.nextInt();
+int[] arr=new int[n];
+for(int i=0;i<n;i++)
+arr[i]=scan.nextInt();
+for(int i=0;i<n;i++)
+{
+if(arr[i]==i)
+{
+System.out.println(arr[i]);
+}
+}
+}
+}
